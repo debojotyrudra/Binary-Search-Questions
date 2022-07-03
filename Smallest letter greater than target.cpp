0ed6652f743da6908c8342bbaebfdf4ac80bb636
debojotyrudra <87115796+debojotyrudra@users.noreply.github.com>
2022-07-03 14:10:21 +0530

    Create Smallest letter greater than target.cpp

---
+++ Smallest letter greater than target.cpp
@@ -0,0 +1,15 @@
+char nextGreatestLetter(vector<char>& letters, char target) {
+        int low=0,high=letters.size()-1;
+        char ans=' ';
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(letters[mid]<=target)
+            {
+                low=mid+1;
+            }
+            else
+                high=mid-1;
+        }
+        return letters[low%letters.size()];
+    }
